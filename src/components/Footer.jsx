2e70ccba68b01671a74b2899dded94d834575504
Jonathan Pynes <jon.pynes@shineforth.co>
2022-05-29 00:17:32 -0500

    TODO font, animations and hovers, other things for sure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+
+  return (
+    <FooterContainer>
+      <div><p>&copy; 2022 jonpynes.com</p></div>
+    </FooterContainer>
+  )
+}
+
+const FooterContainer = styled.div`
+height: 20vh;
+display: flex;
+justify-content: center;
+align-items: flex-end;
+font-size: clamp(0.5rem, 1.5vw + 0.5rem, 1.25rem);
+`
+
+export default Footer;
